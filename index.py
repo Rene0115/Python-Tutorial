035e2e4e2b8513a9a76f094d503f07fede092d64
Rene0115 <enere0115@gmail.com>
2024-03-21 02:13:02 +0100

    added first lines of python code. excellsior!!

---
+++ index.py
@@ -0,0 +1,16 @@
+# Commenting and logging
+print(3)
+
+# Variable declaration
+short_message = "live"
+
+short_message = "dense"
+
+print(short_message)
+
+long_message = """I am  2 live
+long hood"""
+
+
+# Check length of string
+print(len(long_message))
