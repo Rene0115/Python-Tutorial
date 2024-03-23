f8a7d88837994dcef9ea00d5ab60fc0f9224e268
Rene0115 <enere0115@gmail.com>
2024-03-23 02:40:33 +0100

    further studies

--- index.py
+++ index.py
@@ -24,7 +24,7 @@ print(short_message[:3])
 
 
 # print all odd numbers
-new_list = [0,1,2,3,4,5,6,7,8]
+new_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 print(new_list[1:9:2])
 
@@ -35,6 +35,36 @@ print(new_list[8::-1])
 print(short_message.upper())
 
 
-sentence = "{} {}.".format(short_message, long_message)
-print(sentence) #dense I am  2 live
-#long hood.
+sentence = f"{short_message.upper()} {long_message.lower()}."
+print(sentence)  # dense I am  2 live
+# long hood.
+
+
+# multiline comment :
+""" 
+I am alive
+"""
+
+# Working with lists(arrays)
+
+lists = ["here", "there", "everywhere"]
+## basicaly array.push
+lists.append("Art")
+print(lists)
+
+
+#  insert an element to the index of a list
+lists.insert(0, "First")
+print(lists)  # ['First', 'here', 'there', 'everywhere', 'Art']
+
+lists2 = ["list", 2, 4]
+
+# adds elements of a list to the back of another list
+
+lists.extend(lists2)
+print(lists)  # ['First', 'here', 'there', 'everywhere', 'Art', 'list', 2, 4]
+
+# remove an element from a list
+
+lists.remove("here")
+print(lists)  # ['First', 'there', 'everywhere', 'Art', 'list', 2, 4]
